e8ef4f3858b555d7008a80f94fbc5667b7f42e94
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-06-06 12:14:29 +0900

    Updated files

--- while loop.py
+++ while loop.py
@@ -6,3 +6,18 @@
 #     numbers=numbers//10
 # print(total)
 
+# counter=1
+#
+# while counter!=11:
+#     print(counter)
+#     print('Jubaiyer')
+#
+#     counter=counter+2
+
+# i=1
+# count=0
+# while i<=100:
+#     if i%2==0:
+#         count=count+1
+#     i=i+1
+# print(count)
